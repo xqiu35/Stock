7aea6641bebd9bccd95a9ff973c9362c7878b029
xqiu <xqiu@microstrategy.com>
2017-10-19 19:30:17 -0400

    good

--- simple_forecasting/processing.py
+++ simple_forecasting/processing.py
@@ -8,6 +8,7 @@ Created on Tue May 17 15:37:40 2016
 import numpy as np
 from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error, classification_report
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pylab as plt
 import datetime as dt
 import time
@@ -39,13 +40,25 @@ def load_snp_close():
 
     return raw_data, raw_dates
 
+def ts_split(data,pct):
+    train = data[0:int(len(data)*pct)]
+    test = data[int(len(data)*pct):]
+    return train,test
+
+def feature_scaling(data):
+    s = StandardScaler()
+    data_s = s.fit_transform(data)
+    return data_s
+    
+    
 
 def split_into_chunks(data, train, predict, step, binary=True):
     X, Y = [], []
+    #data = np.array(data)
     for i in range(0, len(data),step):
         try:
             x_i = data[i:i+train]
-            y_i = data[i+train+predict]
+            y_i = data[i+train+predict-1]
             x_i,y_i = np.array(x_i), np.array(y_i)
             # Use it only for daily return time series
             if binary:
@@ -96,11 +109,26 @@ def create_Xt_Yt(X, y, percentage=0.8,scale=True,binary=False):
 
 def get_data(s):
     alpha = Alpha.Alpha()
-    data=alpha.GetData(function='TIME_SERIES_INTRADAY',symbol=s,interval='15min',outputsize='full')
+    data=alpha.GetData(function='TIME_SERIES_INTRADAY',symbol=s,interval='1min',outputsize='full')
     raw_data=data['close']
     raw_dates=data['timestamp']
+    raw_data = np.array(raw_data)
+    raw_dates = np.array(raw_dates)
     raw_data = np.flip(raw_data,0)
     raw_dates = np.flip(raw_dates,0)
 
     return raw_data, raw_dates
+
+def get_ts(data,windowsize):
+    X = []
+    #data = np.array(data)
+    for i in range(0, len(data)-windowsize+1,1):
+        try:
+            x_i = data[i:i+windowsize]
+            x_i = np.array(x_i)
+        except:
+            break
+        X.append(x_i)        
+    return X
+    
 
--- simple_forecasting/training.py
+++ simple_forecasting/training.py
@@ -10,7 +10,7 @@ from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error, classification_report
 import matplotlib.pylab as plt
 import datetime as dt
-import time
+
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
@@ -18,6 +18,7 @@ from keras.layers.recurrent import LSTM, GRU
 from keras.layers import Convolution1D, MaxPooling1D
 from keras.callbacks import Callback
 import processing as p
+import time
 
 
 class TrainingHistory(Callback):
@@ -38,31 +39,39 @@ class TrainingHistory(Callback):
 
 
 
-TRAIN_SIZE = 2
-TARGET_TIME = 1
+WINDOW_SIZE = 30
+TARGET_TIME = 15
 LAG_SIZE = 1
 EMB_SIZE = 1
+TRAIN_RATIO = 0.8
 
+
+################################  Retrieve Data #############################################
 print('Data loading..')
-timeseries, dates = p.get_data('APPL')
+data, dates = p.get_data('BABA')
 dates = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S').date() for d in dates]
 #plt.plot(dates, timeseries)
 
-X, Y = p.split_into_chunks(timeseries, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False)
-X, Y = np.array(X), np.array(Y)
-X_train, X_test, Y_train, Y_test = p.create_Xt_Yt(X, Y, percentage=0.9,scale=True)
+################################  Preprocess Data #############################################
+train,test = p.ts_split(data,TRAIN_RATIO)
+#train = p.feature_scaling(train)
 
-Xp, Yp = p.split_into_chunks(timeseries, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False)
-Xp, Yp = np.array(Xp), np.array(Yp)
-X_trainp, X_testp, Y_trainp, Y_testp = p.create_Xt_Yt(Xp, Yp, percentage=0.9,scale=False)
+################################  Training Set #############################################
+X_train, Y_train = p.split_into_chunks(train, WINDOW_SIZE, TARGET_TIME, LAG_SIZE, binary=False)
+X_train, Y_train = np.array(X_train), np.array(Y_train)
 
+################################  Testing Set #############################################
+X_test, Y_test = p.split_into_chunks(test, WINDOW_SIZE, TARGET_TIME, LAG_SIZE, binary=False)
+X_test, Y_test = np.array(X_test), np.array(Y_test)
+#X_test = p.feature_scaling(X_test)
 
+################################ Model #############################################
 print('Building model...')
 model = Sequential()
-model.add(Dense(256, input_shape = (TRAIN_SIZE, )))
+model.add(Dense(512, input_shape = (WINDOW_SIZE, )))
 model.add(Activation('relu'))
 model.add(Dropout(0.25))
-model.add(Dense(128))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dense(1))
 model.add(Activation('linear'))
@@ -75,41 +84,57 @@ model.fit(X_train,
           batch_size = 128, 
           verbose=1, 
           validation_split=0.1)
-score = model.evaluate(X_test, Y_test, batch_size=64)
-print(score)
+#score = model.evaluate(X_test, Y_test, batch_size=128)
+#print(score)
+
+################################ Prediction #############################################
+ts_test = p.get_ts(test,WINDOW_SIZE)
+ts_test = np.array(ts_test)
+predicted = model.predict(ts_test)
+predicted = predicted+(test[2*TARGET_TIME-1]- predicted[0])
 
 
-params = []
-for xt in Y_trainp:
-    xt = np.array(xt)
-    mean_ = xt.mean()
-    scale_ = xt.std()
-    params.append([mean_, scale_])
-
-predicted = model.predict(X_test)
-new_predicted = []
-
-for pred, par in zip(predicted, params):
-    a = pred*par[1]
-    a += par[0]
-    new_predicted.append(a)
-    
-
-mse = mean_squared_error(predicted, new_predicted)
-print(mse)
-
-predicted = predicted+(Y_testp[0]- predicted[0])
-new_predicted = new_predicted+((Y_testp[0]- new_predicted[0]))
-
+################################ Predict All #############################################
+#predicted_all = model.predict(np.reshape(test[:10],(-1,5)))
+#predicted_all = predicted_all+(Y_test[0]- predicted_all[0])
 try:
     fig = plt.figure()
     #plt.plot(Y_test[:150], color='black') # BLUE - trained RESULT
-    #plt.plot(predicted[:150], color='blue') # RED - trained PREDICTION
-    plt.plot(Y_testp[:150], color='green') # GREEN - actual RESULT
-    plt.plot(new_predicted[:150], color='red') # ORANGE - restored PREDICTION
+    #plt.plot(predicted_all, color='blue') # RED - trained PREDICTION
+    #plt.plot(test, color='red') # RED - trained PREDICTION
+    x_real = np.linspace(1,1000,num=1000)
+    plt.plot(x_real[0:len(test)],test[:], color='green') # GREEN - actual RESULT
+    plt.plot(x_real[0:len(predicted)]+WINDOW_SIZE+TARGET_TIME-1,predicted[:], color='red') # ORANGE - restored PREDICTION
+    #plt.xticks(np.arange(min(x_real), max(x_real)+1, 2.0))
+    plt.axis([-50, 800, 174, 181])
     plt.show()
 except Exception as e:
     print(str(e))
     
-profit = (predicted[-1]-X_testp[-1][-1])*100/X_testp[-1][-1]
+################################ Predict Test #############################################
+fig = plt.figure()
+ax = fig.add_subplot(111)
+xxx = ax.plot(range(0,2))[0]
+ax.relim() 
+ax.autoscale_view(True,True,True)
+fig.canvas.draw()
+plt.show(block=False)
+xxx.set_xdata(range(2,4))
+fig.canvas.draw()
+for i in range (WINDOW_SIZE-1,len(test)):
+    try:
+        #plt.plot(Y_test[:150], color='black') # BLUE - trained RESULT
+        #plt.plot(predicted_all, color='blue') # RED - trained PREDICTION
+        #plt.plot(test, color='red') # RED - trained PREDICTION
+        plt.plot(x_real[0:i],test[0:i], color='green') # GREEN - actual RESULT
+        plt.plot(x_real[0:i-WINDOW_SIZE+1]+WINDOW_SIZE+TARGET_TIME-1,predicted[0:i-WINDOW_SIZE+1], color='red') # ORANGE - restored PREDICTION
+        #plt.xticks(np.arange(min(x_real), max(x_real)+1, 2.0))
+        plt.axis([-50, 800, 174, 181])
+        time.sleep(1)
+    except Exception as e:
+        print(str(e))
+        
+
+    
+profit = (predicted[-1]-test[-1])*100/X_test[-1][-1]
 print("score:",profit)
